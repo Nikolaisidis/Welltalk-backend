33cf8d235d5634e14653763728f8edb21f345c69
Alicetair Cyric Toyoca <alestaircyrilculanag.coyoca@cit.edu>
2024-10-03 17:23:30 +0800

    websocket

--- welltalk/src/main/java/com/communicators/welltalk/config/WebSocketConfig.java
+++ welltalk/src/main/java/com/communicators/welltalk/config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("https://well-talk-ten.vercel.app")
                 .withSockJS();
     }
 
