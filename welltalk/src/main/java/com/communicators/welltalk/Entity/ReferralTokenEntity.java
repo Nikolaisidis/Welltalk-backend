72e92448f7ea604f2e4554292c0f896ce6409ac3
Nikolaisidis <146918297+Nikolaisidis@users.noreply.github.com>
2024-11-26 13:51:10 +0800

    kil

--- welltalk/src/main/java/com/communicators/welltalk/Entity/ReferralTokenEntity.java
+++ welltalk/src/main/java/com/communicators/welltalk/Entity/ReferralTokenEntity.java
@@ -1,21 +1,21 @@
 package com.communicators.welltalk.Entity;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.OneToOne;
 import javax.persistence.JoinColumn;
-import javax.persistence.FetchType;
-
-import java.util.Date;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "referral_token")
 public class ReferralTokenEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String token;
