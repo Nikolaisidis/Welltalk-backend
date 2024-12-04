aa15e9f5aa61d181ae6322800c6ddb3d12647e6c
joshuabriones <joshua.briones@cit.edu>
2024-12-04 20:05:31 +0800

    Counselor: date blocker

--- welltalk/src/main/java/com/communicators/welltalk/Entity/CounselorEntity.java
+++ welltalk/src/main/java/com/communicators/welltalk/Entity/CounselorEntity.java
@@ -11,20 +11,30 @@ public class CounselorEntity extends UserEntity {
     @Column(name = "assignedYear")
     private String assignedYear;
 
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "unavailableDates")
+    private String unavailableDates; 
+
     public CounselorEntity() {
 
     }
 
     public CounselorEntity(String institutionalEmail, String idNumber, String firstName, String lastName, String gender,
-            String password, String image, String role, String assignedYear, String college, String program) {
+            String password, String image, String role, String assignedYear, String college, String program, String status, String unavailableDates) {
         super(institutionalEmail, idNumber, firstName, lastName, gender, password, image, role, college, program);
         this.assignedYear = assignedYear;
+        this.status = status;
+        this.unavailableDates = unavailableDates;
     }
 
     public CounselorEntity(int teacherId, String institutionalEmail, String idNumber, String firstName, String lastName,
-            String gender, String password, String role, String assignedYear, String college, String program) {
+            String gender, String password, String role, String assignedYear, String college, String program, String status, String unavailableDates) {
         super(institutionalEmail, idNumber, firstName, lastName, gender, password, role, college, program);
         this.assignedYear = assignedYear;
+        this.status = status;
+        this.unavailableDates = unavailableDates;
     }
 
     public String getAssignedYear() {
@@ -33,5 +43,21 @@ public class CounselorEntity extends UserEntity {
 
     public void setAssignedYear(String assignedYear) {
         this.assignedYear = assignedYear;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUnavailableDates() {
+        return unavailableDates;
+    }
+
+    public void setUnavailableDates(String unavailableDates) {
+        this.unavailableDates = unavailableDates;
     }
 }
